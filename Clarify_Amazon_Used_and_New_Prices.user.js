bfbded4855c6e70a74d876665e7a5393ee0988d4
Andrew Moss <mossy1278@gmail.com>
2013-06-03 17:14:33 +0200

    Removed filling in of 'more buying choices'. Vastly improved globalisation and formatting using Globalize.js. Fixes #2, Fixes #10. Fixed missed page formats (fixes #12, fixes #14).

--- Clarify_Amazon_Used_and_New_Prices.user.js
+++ Clarify_Amazon_Used_and_New_Prices.user.js
@@ -1,24 +1,50 @@
 // ==UserScript==
 // @name        Clarify Amazon Used and New Prices
-// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js 
+// @require     https://raw.github.com/jquery/globalize/10b6de8cfbb7458d23bbe25f662f22624a00fe31/lib/globalize.js
+// @require     https://raw.github.com/jquery/globalize/10b6de8cfbb7458d23bbe25f662f22624a00fe31/lib/cultures/globalize.culture.en-US.js
+// @require     https://raw.github.com/jquery/globalize/10b6de8cfbb7458d23bbe25f662f22624a00fe31/lib/cultures/globalize.culture.en-CA.js
+// @require     https://raw.github.com/jquery/globalize/10b6de8cfbb7458d23bbe25f662f22624a00fe31/lib/cultures/globalize.culture.en-GB.js
+// @require     https://raw.github.com/jquery/globalize/10b6de8cfbb7458d23bbe25f662f22624a00fe31/lib/cultures/globalize.culture.fr-FR.js
+// @require     https://raw.github.com/jquery/globalize/10b6de8cfbb7458d23bbe25f662f22624a00fe31/lib/cultures/globalize.culture.de-DE.js
+// @require     https://raw.github.com/jquery/globalize/10b6de8cfbb7458d23bbe25f662f22624a00fe31/lib/cultures/globalize.culture.it-IT.js
+// @require     https://raw.github.com/jquery/globalize/10b6de8cfbb7458d23bbe25f662f22624a00fe31/lib/cultures/globalize.culture.es-ES.js
 // @namespace   471461
 // @description Update the price for 'used and new' items on the product page to include delivery/p&p. Shows each seller's full price (including delivery/p&p) on the Amazon 'used and new' tab. Shows prices inc delivery in search results too.
-// @include     /^https?://www\.amazon\.((com|ca|co\.uk)|(fr|de|it|es))/((gp/(offer-listing|product)/)|(.+?/dp/.+?/|dp)|s/).*$/
+// @include     /^https?://www\.amazon\.(com|ca|co\.uk|fr|de|it|es)/((gp/(offer-listing|product)/)|(.+?/dp/.+?|dp)|s/).*$/
 // @version     2.02
-// @grant none
+// @grant GM_addStyle
 // ==/UserScript==
+// Grant needed to workaround http://stackoverflow.com/questions/12146445/jquery-in-greasemonkey-1-0-conflicts-with-websites-using-jquery
 
 // Disclaimer: Neither the author or script are in any way affiliated with Amazon.com, Inc.
 
 jQuery(document).ready(function($) {
-	// Match the tld of current amazon sites, does not currently function for jp and cn
-	var tldRegex = /www\.amazon\.((com|ca|co\.uk)|(fr|de|it|es))/;
-	// Check whether to use dot separated decimal float values or the european comma separated style
-	var isDotFloats = typeof tldRegex.exec(window.location.host)[3] === "undefined";
+    // Locales for each of the amazon sites
+    var locales = {'com': 'en-US',
+    'ca': 'en-CA',
+    'co.uk': 'en-GB',
+    'fr': 'fr-FR',
+    'de': 'de-DE',
+    'it': 'it-IT',
+    'es': 'es-ES'};
+
+    // Amazon formats EUR differently than the default, change the european locales to reflect this
+    var euroLocales = ['fr', 'de', 'it', 'es'];
+    for( var i = 0; i < euroLocales.length; i++ ) {
+        var loc = Globalize.culture( locales[euroLocales[i]] );
+        loc.numberFormat.currency.symbol = 'EUR';
+        loc.numberFormat.currency.pattern = ['$ -n', '$ n'];
+    }
+
 	// Identifier stem for certain page elements added by this script
 	var clarifyStem = 'clarify_grease_';
 	// Regex to match the used and new page containing prices for a specific condition
 	var usedNewUrlRegex = /^.*?\/gp\/offer-listing\/(.+?)\/.+condition=([^&]+)/;
+
+    // Determine which locale to used based on the TLD
+    var locale = locales[/www\.amazon\.([^\/]+)/.exec(window.location.host)[1]];
+    Globalize.culture(locale);
 	
 	// Check page type to perform appropriate action
 	if( window.location.pathname.indexOf('/gp/offer-listing') === 0 ) {
@@ -79,9 +105,6 @@ jQuery(document).ready(function($) {
 				dataType: 'html',
 				success: function(data) {
 					var parseResult = parseFirstRow(data);
-					if( parseResult !== null ) {
-						$('span.price', elem).html($('span.price', elem).html() + ' <i>(' + parseResult.currencySym + parseResult.totalPrice + ' inc. P&P)</i>');
-					}
 				},
 				complete: function() {
 					removeLoadingText();
@@ -91,110 +114,145 @@ jQuery(document).ready(function($) {
 	}
 	
 	function usedNewPage() {
-		$('tbody.result').each(function() {
-			var parseResult = parsePriceRow(this);
-			if( parseResult !== null ) {
-				$('span.price', this).html($('span.price', this).html() + ' <i>(' + parseResult.currencySym + parseResult.totalPrice + ' inc. P&P)</i>');
-			}
+		$('tbody.result, div.olpOffer').each(function() {
+			parseAndUpdatePriceRow(this);
 		});
+
 	}
+
+    function getPriceHtml(parseResult) {
+        return ' <i>(' + Globalize.format(parseResult.totalPrice, 'c') + ' inc. P&P)</i>';
+    }
+
+    // Event handler for when price is fetched, object will be updated with incHtml which is the price addition
+    function updatePrice(obj, incHtml) {
+            if(obj.hasClass('buyAction')) {
+                // Text is not actually stored in the link for these
+                $('span.price', obj.parent()).html($('span.price', obj.parent()).html() + incHtml);
+            } else {
+                obj.html(obj.html() + incHtml);
+            }
+    }
 	
 	function productPage() {
-		var lowestPrice = null;
-		$('#olpDivId span.olpCondLink').each(function() {
-			var olpCondLink = this;
-			$.ajax({
-				url: $('a.buyAction', this).attr('href'),
-				dataType: 'html',
-				success: function(data) {
-					// Get the total price from the first row of the used/new page
-					var fetchedUrl = this.url;
-					var parseResult = parseFirstRow(data);
-					
-					if( parseResult !== null ) {						
-						// Create the HTML to append to bare prices
-						var incHtml = ' <i>(' + parseResult.currencySym + parseResult.totalPrice + ' inc. P&P)</i>';
-						
-						// Update the price on the product page to be the total price
-						$('span.price', olpCondLink).html($('span.price', olpCondLink).html() + incHtml);
-							
-						// Match the product code and condition
-						var urlMatchFetched = usedNewUrlRegex.exec(fetchedUrl);
-											
-						// Update the appropriate price in the central table, checking the product code and condition to determine the correct cell
-						$('div.cBoxInner td.tmm_olpLinks a').each(function() {
-							var urlMatchCell = usedNewUrlRegex.exec($(this).attr('href'));
-							if( urlMatchFetched[1] === urlMatchCell[1] && urlMatchFetched[2] === urlMatchCell[2] ) {
-								$(this).html($(this).html() + incHtml);
-							}
-						});
-						
-						// Update any prices in the secondary used and new table (right hand side of page)
-						$('div#secondaryUsedAndNew div.mbcOlpLink').each(function() {
-							var olpLinkDiv = $(this);
-							$('a', olpLinkDiv).each(function() {
-								var urlMatchCell = usedNewUrlRegex.exec($(this).attr('href'));
-								if( urlMatchFetched[1] === urlMatchCell[1] && urlMatchFetched[2] === urlMatchCell[2] ) {
-									$('span.price', olpLinkDiv).html($('span.price', olpLinkDiv).html() + incHtml);
-								} else if( urlMatchCell[2] === 'all' ) {
-									// Sometimes there is a 'used & new' link, so in this case update it with the lowest of the used/new prices
-									// Keep track of the lowest price of used or new, and update the 'used and new' price if there is a new lower one
-									if( lowestPrice === null || parseResult.totalPrice < lowestPrice ) {
-										lowestPrice = parseResult.totalPrice;
-										$('span.price', olpLinkDiv).html(parseResult.currencySym + parseResult.price + incHtml);
-									}
-								}
-							});
-						});
-					}
-				}
-			});
-		});
+        // Keyed by productID_condition
+        var prodObjs = {};
+		
+        // Get all the URLs that need to be fetched and which objects need to be updated
+        // for AJAX event handling and to ensure pages aren't fetched more than once unnecessarily
+        $('#olpDivId span.olpCondLink > a, td.tmm_olpLinks > a').each(function() {
+            // Attempt to match the productID and condition in the URL
+            var fetchedUrl = $(this).attr('href');
+            var urlMatchFetched = usedNewUrlRegex.exec(fetchedUrl);
+            if( urlMatchFetched === null ) {
+                return;
+            }
+            var key = urlMatchFetched[1] + '_' + urlMatchFetched[2];
+
+            if( typeof(prodObjs[key]) === 'undefined' ) {
+                prodObjs[key] = {};
+                prodObjs[key].elems = [];
+                prodObjs[key].url = fetchedUrl;
+            } 
+
+            // This element will need to be updated
+            prodObjs[key].elems.push($(this));
+        });
+
+        var ajaxCalls = [];
+        // Get all the prices necessary
+        $.each(prodObjs, function(key, obj) {
+            // Get the price information from the usednew page
+            ajaxCalls.push($.ajax({
+                url: obj.url,
+                dataType: 'html',
+                success: function(data) {
+                    // Get the total price from the first row of the used/new page
+                    var parseResult = parseFirstRow(data);
+                    
+                    if( parseResult !== null ) {						
+                        obj.totalPrice = parseResult.totalPrice;
+                        for( var i = 0; i < obj.elems.length; i++ ) {
+                            // Update prices on page
+                            updatePrice( obj.elems[i], getPriceHtml(parseResult) );
+                        }
+                    }
+                }
+            }));
+        });
+
+        // Highlight lowest used/new prices
+        // Thanks to http://stackoverflow.com/a/5627301/727357 for when syntax
+        $.when.apply(null, ajaxCalls).done(function() {
+            // Find minimum price
+            var minKey = null;
+            var min = Number.MAX_VALUE;
+            $.each(prodObjs, function(key, obj) {
+                if( obj.totalPrice < min ) {
+                    minKey = key;
+                    min = obj.totalPrice;
+                }
+            });
+            
+            var obj = prodObjs[minKey];
+            // Highlight lowest prices
+            for( var i = 0; i < obj.elems.length; i++ ) {
+                var elem = obj.elems[i];
+                if(elem.hasClass('buyAction')) {
+                    $('span.price', elem.parent()).html('<b>' + $('span.price', elem.parent()).html() + '</b>');
+                } else {
+                    elem.html('<b>' + elem.html() + '</b>');
+                }
+            }
+        });
 	}
 	
 	// Convenience method to parse the first price row of the used and new page
 	function parseFirstRow(pageHtml) {
 		var page = $(pageHtml);
-		if( $('tbody.result', page).length < 1 ) {
+        var rows = $('tbody.result, div.olpOffer', page);
+		if( rows.length < 1 ) {
 			return null;
 		}
-		var row = $('tbody.result', page)[0];
-		return parsePriceRow(row);
+		var row = rows[0];
+		return parseAndUpdatePriceRow(row);
 	}
 	
 	// Parse the price and currency symbol from a row in the used and new page table
-	function parsePriceRow(row) {
-		// Skip rows without a price specified
-		if( $('span.price', row).html() === null ) {
-			return null;
-		}
+	function parseAndUpdatePriceRow(row) {
+        var priceSpan = null;
+        var priceShippingSpan = null;
+
+        // Match different styles of usednew pages
+		if( $('span.price', row).length > 0 ) {
+            priceSpan = $('span.price', row);
+            priceShippingSpan = $('span.price_shipping', row);
+		} else if( $('span.olpOfferPrice', row).length > 0 ) {
+            priceSpan = $('span.olpOfferPrice', row);
+            priceShippingSpan = $('span.olpShippingPrice', row);
+        } else {
+            return null;
+        }
 		
 		// In the case of items with no delivery price specified (eg. Amazon fulfilled) assume free delivery
-		if( $('span.price_shipping', row).html() === null ) {
-			var priceDelivery = 0;
-		}
+        var priceDelivery = 0;
 	
 		// Determine the currency symbol from the start of the price string
-		var currencySym = /(^\D+)/.exec($('span.price', row).html())[1];
+        var priceRegex = /^\s*(^\D+)(\S+)/;
+        var priceMatch = priceRegex.exec(priceSpan.html());
 	
-		if( isDotFloats ) {
-			var stripRe = /[^\.\d]*/g;
-			var price = parseFloat($('span.price', row).html().replace(stripRe, ''));
-			if( typeof priceDelivery === 'undefined' ) {
-				var priceDelivery = parseFloat($('span.price_shipping', row).html().replace(stripRe, ''));
-			}
-		} else {
-			var stripRe = /[^,\d]*/g;
-			var price = parseFloat($('span.price', row).html().replace(stripRe, '').replace(',', '.'));
-			if( typeof priceDelivery === 'undefined' ) {
-				var priceDelivery = parseFloat($('span.price_shipping', row).html().replace(stripRe, '').replace(',', '.'));
-			}
-		}
-		
-		return {
-			'price': price.toFixed(2),
-			'totalPrice': (price + priceDelivery).toFixed(2),
-			'currencySym': currencySym
+        var price = Globalize.parseFloat(priceMatch[2]);
+		if( priceShippingSpan.length > 0 ) {
+            priceDelivery = Globalize.parseFloat(priceRegex.exec(priceShippingSpan.html())[2]);
+        }
+
+		var parseResult = {
+			'price': price,
+			'totalPrice': price + priceDelivery, 
 		};
+
+        priceSpan.html(priceSpan.html() + getPriceHtml(parseResult));
+
+        return parseResult;
 	}
 });
